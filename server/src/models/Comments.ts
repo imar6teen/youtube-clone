552287b61dcf59e10027a7562a364ceaa8e28a5e
Aimar Anand <aimaranand02@gmail.com>
2024-01-05 18:24:20 +0800

    server:add: adding comment

--- server/src/models/Comments.ts
+++ server/src/models/Comments.ts
@@ -11,10 +11,12 @@ const CommentsSchema = new mongoose.Schema({
     ref: "Users",
     required: true,
   },
+  // if not exist, that means the comment is root
+  // reply_to is tag for the comment that replied
   reply_to: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "Users",
-    required: true,
+    ref: "Comments",
+    // required: true,
   },
   comment: {
     type: String,
